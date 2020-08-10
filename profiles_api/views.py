ca37895d17cb75acf4d5a48e0b2799037f3212ba
Leonardo Black <leonardoblack.net@gmail.com>
2020-08-10 16:30:50 -0300

    Implement Searchfield to UserProfileList

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,6 @@
 from profiles_api import models
 from rest_framework import status
+from rest_framework import filters
 from rest_framework import viewsets
 from profiles_api import serializers
 from profiles_api import permissions
@@ -108,5 +109,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email',)
