4d82306c436848b5c6f75ca7324bc4b14cd9bb9e
Leonardo Black <leonardoblack.net@gmail.com>
2020-08-10 16:53:11 -0300

    Implemented the function that create auth tokens to users

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,12 +1,16 @@
-from profiles_api import models
 from rest_framework import status
 from rest_framework import filters
 from rest_framework import viewsets
-from profiles_api import serializers
-from profiles_api import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+from profiles_api import models
+from profiles_api import serializers
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -113,5 +117,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
 
-    filter_backends = (filters.SearchFilter, )
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
